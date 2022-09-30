bd6d5863df90744fbe3937cedf8f9bc993b89ffb
austonyb <auston.youngblood@gmail.com>
2022-09-30 10:15:22 -0600

    Finished sdlc.js

--- sdlc.js
+++ sdlc.js
@@ -23,10 +23,10 @@ const planning = "This is the first step, in which the end goal of the software 
 
 const analysisOfRequirements = "This is the second step of the Software Development Lifecycle. At this point, developers will determine (based on the planning) what tech stacks need to be used for the planned product and/or features, and how much time and staff will be needed to work on the project."
 
-const design = "This is the third stage of the Software Development Lifecycle. At this stage, the developers will design the architecture for the project and decide on the "
+const design = "This is the third stage of the Software Development Lifecycle. At this stage, the developers will design the architecture for the product and how all of the features and aspects of the product or service will work with each other."
 
-const implementation = ""
+const implementation = "The fourth stage of the Software Development Lifecycle. This is where the rubber hits the road, so to speak. The actual coding and building of the project takes place in this step."
 
-const testingAndIntegration = ""
+const testingAndIntegration = "This is the fifth stage of the Software Development Lifecycle. In this stage the product will be tested and worked on until it is determined that it has reached the status of MVP (minimal viable product). At such point, the product will be released or deployed."
 
-const maintain = ""
+const maintain = "The sixth and final stage of the Software Development Lifecycle; this is where general maintainance of the service or product is taken care of---really anything related to keeping the product live and working. If new features to the product or service are desired, the cycle usually starts over to implement them."
